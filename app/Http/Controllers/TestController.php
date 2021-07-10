b5e06b83b6d65d9e6b05257931df6a8aae19dee1
Moses Wise <moseswisegit@gmail.com>
2021-07-10 06:26:56 +0100

    install vue

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function affichage(){
+       return response()->json([
+           'msg' => 'un message en json'
+       ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+ Route::get('/','TestController@affichage');
+
+// Route::any('{slug}', function () {
+//     return view('welcome');
+// });
